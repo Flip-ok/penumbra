e7bdc52b2f6346124abf96b4219253373eeb7056
Flip-ok <96481046+Flip-ok@users.noreply.github.com>
2022-08-03 14:02:50 +0300

    Update TurbokomaPenumbradvanol.sh

--- TurbokomaPenumbradvanol.sh
+++ TurbokomaPenumbradvanol.sh
@@ -4,5 +4,3 @@ git clone https://github.com/penumbra-zone/penumbra
 cd penumbra && git fetch && git checkout 023-arche.1 && cargo update
 cargo build --release --bin pcli
 cargo run --release --bin pcli wallet reset
-echo -e " 
-Tʜɪs ᴍᴇssᴀɢᴇ ɪs ᴏɴʟʏ ᴠɪsɪʙʟᴇ ᴛᴏ ɢᴀʏs. "
